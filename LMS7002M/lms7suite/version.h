60f2232e026a23807dedd14fef0a6b7a5eab3864
rjonaitis <richard.jonaitis@gmail.com>
2015-04-03 15:04:10 +0300

    added lms7suite project

---
+++ LMS7002M/lms7suite/version.h
@@ -0,0 +1,16 @@
+/**
+@author Lime Microsystems
+@brief  Automatically generated software version
+*/
+#pragma once
+namespace AutoVersion
+{
+    static const int year = 2015;
+    static const int month = 4;
+    static const int day = 3;
+    static const int hour = 14;
+    static const int minutes = 22;
+    static const int seconds = 24;
+    static const int buildsCounter = 1;
+    static const char branchName[] = "";
+}
